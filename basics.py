613ceda1f200c30bb2ac26d378631c6da5a994f1
Chia-Yang Tsai <79623264+chiayangtsai@users.noreply.github.com>
2025-08-09 04:52:52 +0000

    class material and HW0808

---
+++ basics.py
@@ -0,0 +1,89 @@
+def basic_bubble_sort():
+    #Q: use bubble sort to complete ascending sort results
+    a = [3, 5, 1, 4, 2]
+    #time complexity : O(N^2)
+    for i in range(len(a) - 1):
+        for j in range(i + 1, len(a)):
+            if a[i] > a[j]:
+                a[i], a[j] = a[j], a[i]
+
+    print(a)
+
+
+def basic_time_complexity():
+    # execution time response vs input data size (N)
+    #  Big-O function : O(N)
+    #                   O(N^2) : 2nd order
+    #                   O(1) : data size irrelevant
+    return
+
+
+def basic_list_i():
+
+    #-- memory ---
+    a = [3, 5, 1, 4, 2]  #<== vector/array
+    #    0  1  2  3  4
+    # a represent the "memory address" of the elements in the list
+
+    # -- indexing
+    #Q: print out the index-2 element => 1
+    print(a[2])
+
+    # -- last index
+    #Q: print out the last index element => 2
+    print(a[-1])
+    print(a[len(a) - 1])
+
+    # --- + : connect /cascade
+    a = [3, 1, 2]
+    b = [1, 2]
+    #Q: append b to a
+    a = a + b
+
+    #Q: append a new element -1 to a => [3, 1, 2, -1]
+    a = [3, 1, 2]
+    a = a + [-1]  #拆掉重蓋
+
+    a = [3, 1, 2]
+    a += [-1]  #加蓋
+
+    a = [3, 1, 2]
+    a.append(-1)  #加蓋
+
+
+def leetcode_shuffle_two_lists():
+    a = [3, 5, 1, 4, 2]
+    b = [0, 7, 6]
+    c = []
+    #Q: shuffle a and b to a new list c following index order
+    # c=> [3, 0, 5, 7, 1, 6, 4, 2]
+    #HW0808 : O(N)
+    i, j = 0, 0
+    # while i < len(a) and j < len(b):
+
+    return
+
+
+def leetcode_merge_two_lists():
+    a = [1, 2, 5, 6, 8]
+    b = [0, 3, 4, 7, 10, 11]
+    c = []
+    #Q: Given two sorted lists a and b, merge these two lists to c and c is also sorted
+    # c=> [0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 11]
+    #HW0808 : O(N)
+
+    return
+
+
+def basic_list_ii():
+    a = [5, 4, 1, 3, 2, 9, 8]
+    #    0  1  2  3  4  5  6
+    d = a[2:6:1] # => a[2:6]
+    print("get the new list from index-2 to index-5: %s" % str(d))
+
+    d = a[3:]
+    print("get the new list from index-3 to index-6: %s" % str(d))
+
+    #HW0808(VK) : start from here next time
+    
+    return
---
+++ main.py
@@ -0,0 +1,60 @@
+#windows platform
+
+
+from basics import *
+
+
+if __name__ == '__main__':
+    testID =7
+
+    if testID == 0:
+        print("Hello world")
+    elif testID == 1:
+        basic_bubble_sort() #bubble sort
+    elif testID == 2:
+        basic_time_complexity() #time complexity
+    elif testID == 3:
+        basic_list_i() # list i
+    elif testID == 5:
+        leetcode_shuffle_two_lists()
+    elif testID == 6:
+        leetcode_merge_two_lists()
+    elif testID == 7:
+        basic_list_ii() # list ii
+        
+
+
+'''
+上
+- list_i : indexing, "+"
+- list_ii : ":" operations
+- list_iii : bulit-in method
+- copy : shallow copy, deep copy, mutable/immutable
+- call-by-reference
+- string / ASCII 
+- KES algorithm (LUT)
+- sliding window
+
+中
+- dict : LUT
+- sliding window + LUT
+- decimal, binary representation
+- .sort()
+- string <-> list
+
+下
+- recursive programming | DP principles
+- 2D problems - coordinators conversion - raster-scan, zig-scan order
+
+- leetcode 150 : "easy", "medium", before "stack"
+
+- binary tree concept
+- binary tree - breadth-first (BF) creation
+
+---- ADVANCED LEVEL ---
+- binary tree..
+......
+
+'''
+
+        
